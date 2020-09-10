57ec9156b92a5ff800cb0bffc89f9370cdaff804
Alexey Zelenin <alexey@zelenin.org>
2020-09-10 18:45:27 +0300

    Add logging to socket.io

--- socketio/socketio.js
+++ socketio/socketio.js
@@ -1,17 +1,27 @@
 const socketioServer = require('socket.io')
+const LogFactory =  require('../utils/logFactory')
+const path = require('path');
 
 var socketio = null
+let _log
 
 function getSocketIO(settings) {
 
   if (socketio != null)
     return socketio
 
+  _log = LogFactory.create(path.basename(__filename), settings.Main.LoggingLevel)
+
   const isDisabled = settings.SocketIO.Disabled
   const port = settings.SocketIO.Port
 
   if (!isDisabled)
     socketio = socketioServer.listen(port)
+
+  hostPort = socketio.httpServer._connectionKey
+                            .replace('6:::', 'localhost')
+
+  _log.info(`Listening Socket.IO at http://${hostPort}`)
 
   return socketio
 }
