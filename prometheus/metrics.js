351fa08aae2c8569ee74cba55156fb6da9d725a1
Alexey Zelenin <alexey@zelenin.org>
2021-01-30 22:54:05 +0300

    Improve histogram

--- prometheus/metrics.js
+++ prometheus/metrics.js
@@ -11,7 +11,7 @@ class Metrics {
     static order_book_in_delay = new prometheus.Histogram({
       name: 'order_book_in_delay',
       help: 'Histogram of received order book delay.',
-      buckets: [0.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100, 200, 300, 400, 500, 1000, 2000, 3000, 5000]
+      buckets: [0.1, 1, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 2000, 3000, 5000]
     });
 
     static order_book_in_delay_ms = new prometheus.Gauge({
